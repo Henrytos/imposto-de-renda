5ba8b859c6c02f72d0ba84324412e523307bf796
Henry <franzhenry46@gmail.com>
2023-05-27 17:09:47 -0300

    arrumando o rodapé

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const buttons = document.querySelectorAll('a')
-buttons.forEach(btn => btn.addEventListener('click', verification))
-function verification() {
+const buttons = document.querySelectorAll('.btn')
+buttons.forEach(btn => btn.addEventListener('click', verificationBtnHover))
+function verificationBtnHover() {
     buttons.forEach(btn => btn != this ? btn.id = '' : btn.id = 'focus')
-} 
+}
+const fotterIcons = document.querySelectorAll('footer > span > a')
+fotterIcons.forEach(icon => icon.addEventListener('mouseenter', fotterHover))
+fotterIcons.forEach(icon => icon.addEventListener('mouseleave', fotterNoHover))
+function fotterHover() {
+    const textIcon = document.createElement('span')
+    textIcon.innerText = this.id
+    textIcon.id = `icon.${this.id}`
+    textIcon.classList = 'text-social-midia'
+    this.append(textIcon)
+}
+function fotterNoHover() {
+    const icon = document.getElementById(`icon.${this.id}`)
+    icon.remove()
+}
