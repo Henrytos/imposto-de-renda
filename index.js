c8264b70a04386c74cf5fbbb9164882e58eb1ecd
Henry <franzhenry46@gmail.com>
2023-06-03 15:27:21 -0300

    adicionando menu hamburguinho e responsividade no site

--- index.js
+++ index.js
@@ -17,3 +17,51 @@ function fotterNoHover() {
     const icon = document.getElementById(`icon.${this.id}`)
     icon.remove()
 }
+
+
+const selectElement = document.getElementById("idioma")
+
+const bandeira = document.getElementById('bandeiras')
+
+let click = 0
+selectElement.addEventListener("click", function () {
+    let selectedOption = selectElement.options[selectElement.selectedIndex];
+
+    click++
+    if (selectedOption.value !== '' && click % 2 == 0) {
+        bandeira.src = `assets/imgs/bandeira/${selectedOption.value}.png`
+        console.log("Opção selecionada: " + selectedOption.text + " valor: " + selectedOption.value)
+        colorTextLeanguage(selectedOption)
+        switchLenguage(selectedOption.value)
+    }
+})
+
+function colorTextLeanguage(selectedOption) {
+
+    switch (selectedOption.value) {
+        case "Português":
+            selectElement.style.color = "#4caf50";
+
+            break;
+
+        case "Inglês":
+            textLeanguage.style.color = "#3F51B5";
+
+            break;
+
+        case "Espanhol":
+            selectElement.style.color = "#FFC107";
+
+            break;
+
+        case "Mandarin":
+            selectElement.style.color = "#FF3D00";
+
+            break;
+
+    }
+}
+function switchLenguage(selectedValue) {
+    console.log(selectedValue)
+
+}
