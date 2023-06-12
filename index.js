67b15767cfe388392c60a4083d40b00cdaa85eab
Henry <franzhenry46@gmail.com>
2023-06-11 22:41:58 -0300

    adicionando os idiomas

--- index.js
+++ index.js
@@ -154,12 +154,72 @@ const LanguageSpanish = {
         '2do lote residual: 30 de noviembre',
         '3er lote residual: 30 de diciembre.'
     ],
-    tabla: [
+    tabela: [
         'SALARIO BRUTO MENSUAL', 'TASA', 'DEDUCCIÓN', 'de 0,00 hasta 1.903,98', 'Exento',
         'de 1.903,99 hasta 2.826,65',
         'de 2.826,66 hasta 3.751,05',
         'de 3.751,06 hasta 4.664,68',
         'a partir de 4.664,68'
+    ]
+}
+const LanguageEnglish = {
+    titles: ['Income Tax', 'What is Income Tax (IRPF)?', 'Income Tax Table', 'Location', 'How does Income Tax calculation work?', 'Frequently Asked Questions'],
+    texts: ["Income Tax for Individuals (IRPF) is a tax imposed by the Federal Revenue Service on the income of an individual and is proportional to the amount of that income.", "Regarding the worker's salary, the Income Tax rate to be paid depends on the salary range and the number of dependents.", "To calculate the monthly amount of IRPF to be paid on the salary, these two factors must be taken into consideration.", "Below, you can check the Income Tax table according to each salary range:", "The Deduction column indicates the maximum amount that can be deducted from the Income Tax in the Annual Declaration if you have incurred deductible expenses.", "Therefore, since the government aims to provide healthcare, education, etc. to the population, all expenses you have in these areas can be deducted from the tax you pay.", "Nevertheless, the Income Tax rate is already deducted from your paycheck, and the amount you receive is already net of taxes.", "Welcome to our income tax declaration company! We are happy to assist you in navigating the complex world of taxes and ensuring that you fulfill your tax obligations efficiently and accurately.", "Our company has years of experience in the industry, and our team of accounting experts is ready to assist you at every step of the income tax declaration process. We understand that each situation is unique, which is why we are dedicated to providing personalized service to meet your specific needs.", "Here at our income tax declaration company, we understand that tax compliance is crucial. Our location section is designed to help you make informed decisions and ensure that you comply with applicable tax laws. Trust our services to simplify the income tax declaration process and minimize the stress associated with taxes.", "Contact us today for more information about our services or to schedule a consultation with one of our accounting experts. We look forward to helping you achieve your financial goals and ensuring your tax peace of mind.", "To calculate the Income Tax base, certain deductions need to be made from the gross salary, which are:", "This will give us the amount on which the Income Tax rate will apply. After all these deductions, we will have the net salary.", "It is important to remember that this tax is applicable whenever a person receives income, not necessarily a salary. Therefore, the Income Tax applies to:", "Income Tax is paid by those who earn a salary above R$ 1,903.98 per month or those who have earned profits from investments that are not exempt.", "To calculate Income Tax for 2023, use the Mobills Income Tax Calculator.", "In the calculator, you will enter your gross salary and the number of legal dependents you have. This will give you the amount to be paid in Income Tax.", "Late filers are subject to a fine.", "According to the Federal Revenue Service, the fine is 1% per month on the tax due, capped at 20% of the amount due to be paid. Additionally, the minimum fine amount is R$ 165.74."
+    ],
+    links: ['TABLE', 'LOCATION', 'FAQ', 'CALCULATOR'],
+    list: ['Contribution to INSS;',
+        'Number of legal dependents (R$ 189.59 per dependent);',
+        'Alimony (for those who pay it).',
+        'Salary above the established limit;',
+        'Vacation and 13th salary;',
+        'Benefits such as profit sharing, bonuses, retirement, unemployment insurance, etc.'
+    ],
+    listDoubts: [
+        '1st batch: May 31',
+        '2nd batch: June30',
+        '3rd batch: July 30',
+        '4th batch: August 31',
+        '5th batch: September 30',
+        '1st residual batch: October 29',
+        '2nd residual batch: November 30',
+        '3rd residual batch: December 30.'
+    ],
+    tabela: [
+        'MONTHLY GROSS SALARY', 'TAX RATE', 'DEDUCTION', 'from 0.00 to 1,903.98', 'Exempt',
+        'from 1,903.99 to 2,826.65',
+        'from 2,826.66 to 3,751.05',
+        'from 3,751.06 to 4,664.68',
+        'over 4,664.68'
+    ]
+}
+const LanguageMandarin = {
+    titles: ['所得税', '什么是所得税（个人所得税）？', '所得税表', '地点', '所得税如何计算？', '常见问题'],
+    texts: ["个人所得税（个人所得税）是由联邦税务局对个人收入征收的税款，与收入金额成比例。", "关于工人的工资，应缴纳的个人所得税税率取决于工资范围和扶养人数。", "要计算应缴纳的个人所得税月度金额，必须考虑这两个因素。", "以下是根据每个工资范围的所得税表：", "扣除列指示如果您有可扣除的费用，最多可以从年度申报中扣除的金额。", "因此，由于政府的目标是为人民提供医疗保健、教育等，您在这些领域的所有支出都可以从您所缴纳的税款中扣除。", "尽管如此，个人所得税税率已从您的工资中扣除，您获得的金额已经净扣除了税款。", "欢迎来到我们的个人所得税申报公司！我们很高兴协助您在复杂的税收世界中导航，并确保您高效准确地履行税务义务。", "我们的公司在这个行业有多年的经验，我们的会计专家团队随时准备协助您完成个人所得税申报过程的每一步。我们理解每个情况都是独特的，因此我们致力于提供量身定制的服务，以满足您的特定需求。", "在我们的个人所得税申报公司，我们了解税务合规的重要性。我们的地点部分旨在帮助您做出明智决策，确保您遵守适用的税法。请放心使用我们的服务，简化个人所得税申报流程，并将与税收相关的压力最小化。", "请即刻与我们联系，以获取有关我们的服务的更多信息，或预约与我们的会计专家之一进行咨询。我们期待帮助您实现财务目标并确保您的税务安宁。", "要计算个人所得税的计算基数，需要从总工资中扣除某些费用，这些费用包括：", "这将得到个人所得税税率适用的金额。在所有这些扣除之后，我们将得到净工资。", "重要的是要记住，个人所得税适用于个人获得的任何收入，不仅仅是工资。因此，个人所得税适用于以下情况：", "工资超过每月1, 903.98美元的人；或者通过投资获得的利润不享受免税待遇的人。", "要计算2023年的个人所得税，请使用Mobills个人所得税计算器。", "在计算器中，您将输入您的总工资和您拥有的合法扶养人数。这将给出您应缴纳的个人所得税金额。", "逾期申报者将受到罚款。", "根据联邦税务局的规定，罚款金额为应缴税款的1 %，上限为应缴款项的20 %。此外，罚款的最低金额为165.74美元。"
+    ],
+    links: ['表格', '位置', '常见问题', '计算器'],
+    list: ['缴纳社会保险费；',
+        '法定扶养人数（每人189.59美元）；',
+        '支付的赡养费（适用于付费方）。',
+        '超过规定限额的工资；',
+        '假期和第13薪水；',
+        '福利，如分红、奖金、退休金、失业保险等。'
+    ],
+    listDoubts: [
+        '第一批次：5月31日',
+        '第二批次：6月30日',
+        '第三批次：7月30日',
+        '第四批次：8月31日',
+        '第五批次：9月30日',
+        '第一批次残余：10月29日',
+        '第二批次残余：11月30日',
+        '第三批次残余：12月30日。'
+    ],
+    tabela: [
+        '每月总工资', '税率', '扣除金额', '0.00至1,903.98', '免税',
+        '1,903.99至2,826.65',
+        '2,826.66至3,751.05',
+        '3,751.06至4,664.68',
+        '超过4,664.68'
     ]
 }
 function switchLenguage() {
